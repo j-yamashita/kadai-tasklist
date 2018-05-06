6b44fe5b3210425b4684f8a89a737e78e4424340
jun.yamashita <j.yamashita26@gmail.com>
2018-05-06 08:19:42 +0000

    Add users and tasks relation

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,22 +2,19 @@ class TasksController < ApplicationController
   
   before_action :require_user_logged_in
   
-  
   def index
     # @tasks = Task.all
-    @tasks = Task.all.page(params[:page]) # kaminariを使うため.page(params[:page])を追加
-    
+    # @tasks = Task.all.page(params[:page]) # kaminariを使うため.page(params[:page])を追加
+    @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
   end
-    
     
   def create
     # createアクションに対応するビューは用意しない。出口はredirect_toかrenderのいずれか。
     # new.html.erbのform_for(@task)によって、自動的に送信先が選ばれる。
     # @taskが新規ならcreateが、既存ならupdateが呼ばれる。
-    # http://railsdoc.com/references/form_for
-    # http://ruby-rails.hatenadiary.com/entry/20140727/1406448610
-    # https://qiita.com/shunsuke227ono/items/7accec12eef6d89b0aa9
-    @task = Task.new(task_params)
+    
+    # @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = "投稿されました"
@@ -29,21 +26,26 @@ class TasksController < ApplicationController
   end
   
   def new
-    @task = Task.new
+    # @task = Task.new
+    @task = current_user.tasks.build
   end
   
   def edit
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
+    correct_user
   end
   
   def show
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
+    correct_user
   end
   
   def update
     # updateアクションに対応するビューは用意しない。出口はredirect_toかrenderのいずれか。
     
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
+    correct_user
+    
     if @task.update(task_params)
       flash[:success] = "更新されました"
       redirect_to @task     # /tasks/:idへリダイレクト。showアクションが呼ばれる。
@@ -56,18 +58,29 @@ class TasksController < ApplicationController
   def destroy
     # destroyアクションに対応するビューは用意しない。
     # show.html.erbのリンクから、tasks/:idにDELETEメソッドを送信することで呼ばれる。
-    @task = Task.find(params[:id])
+    
+    # @task = Task.find(params[:id])
+    correct_user
     
     @task.destroy
     flash[:success] = "削除されました"
     redirect_to tasks_url     # /tasks へのリンク。indexアクションへ行く。
   end
     
-  
   private
   
   def task_params
     params.require(:task).permit(:content, :status)    # taskモデルのcontentカラムとstatusカラムのみ受け取る
   end
   
+  def correct_user
+    # /tasks/:id で受け取ったparams[:id]が、現在ログインしているユーザーのtaskのidであるか否かをチェック
+    # 正しければそのtaskを操作対象にするため@taskを返す
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
+  
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+User.create(name:"taro", email:"taro@suzuki.com", password:"taro1234")
+User.create(name:"jiro", email:"jiro@suzuki.com", password:"jiro1234")
+User.create(name:"saburo", email:"saburo@suzuki.com", password:"saburo1234")
+
+
 states = ["未着手", "着手", "完了", "保留", "中止"]
 (1..100).each do |i|
-  Task.create(content: "タスク#{i}", status: states.sample)
+  Task.create(content: "タスク#{i}", status: states.sample, user_id: Random.rand(1..3))
 end
